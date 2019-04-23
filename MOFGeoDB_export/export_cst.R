ca2ee0d89af95fbc306d0bf6b2152b1793ff3f50
swoellauer <swoellauer>
2019-04-23 10:28:03 +0200

    export to GeoPackage

--- MOFGeoDB_export/export_cst.R
+++ MOFGeoDB_export/export_cst.R
@@ -18,3 +18,16 @@ cst <- trees[substr(trees$tree_name, 1,7) == "mof_cst",
 
 # write into the csv folder
 write.csv(cst, "csv/mof_cst.csv", row.names = FALSE)
+
+# create GeoPackage file
+
+# convert to SpatialPointsDataFrame
+sp::coordinates(cst) <- ~easting+northing
+
+# set projection (check if this is correct)
+sp::proj4string(cst) <- sp::CRS("+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")
+
+layer_name <- "mof_cst"
+
+# write GeoPackage file
+rgdal::writeOGR(cst, dsn = paste0("gpkg/", layer_name, ".gpkg"), layer = layer_name, driver = "GPKG", overwrite = TRUE)
